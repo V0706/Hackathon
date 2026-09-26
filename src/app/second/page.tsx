f1a8e38265749e255c8af08a719b37e115619738
Ar0lona <nike@borheyer.de>
2026-09-26 11:17:51 +0200

    wetter

--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -2,22 +2,24 @@ import Link from "next/link";
 
 export default function SecondPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-900 px-6 py-16 text-center text-white">
+    <main className="flex min-h-screen flex-col items-center justify-start bg-zinc-900 px-6 pb-16 pt-5 text-center text-white">
       <h1 className="mb-6 text-4xl font-bold">Aktuelle Wetterdaten</h1>
       <p className="mb-8 max-w-md text-lg text-zinc-300">
        Die aktuellen Wetterdaten aus dem Wald:
       </p>
-      <div>
-        <p>Temperatur:</p>
-      </div>
-      <div>
-        <p>Luftfeuchtigkeit:</p>
-      </div>
-      <div>
-        <p>Bodenfeuchtigkeit:</p>
-      </div>
-      <div>
-        <p>Helligkeit:</p>
+      <div className="grid grid-cols-2 gap-4 text-left">
+        <div className="flex h-[150px] w-[300px] items-start justify-start rounded-lg bg-blue-500 p-3">
+          <p>Temperatur:</p>
+        </div>
+        <div className="flex h-[150px] w-[300px] items-start justify-start rounded-lg bg-blue-500 p-3">
+          <p>Helligkeit:</p>
+        </div>
+        <div className="flex h-[150px] w-[300px] items-start justify-start rounded-lg bg-blue-500 p-3">
+          <p>Luftfeuchtigkeit:</p>
+        </div>
+        <div className="flex h-[150px] w-[300px] items-start justify-start rounded-lg bg-blue-500 p-3">
+          <p>Bodenfeuchtigkeit:</p>
+        </div>
       </div>
       <div>
         <p></p>
@@ -28,7 +30,7 @@ export default function SecondPage() {
 
       <Link
         href="/"
-       className="rounded-full bg-white px-6 py-3 text-black transition hover:bg-zinc-300"
+       className="mt-[10px] rounded-full bg-white px-6 py-3 text-black transition hover:bg-zinc-300"
       >
         Zurück zur Hauptseite
       </Link>
