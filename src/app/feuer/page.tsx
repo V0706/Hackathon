0c59b5bf1d388f06eaf7c377bd99c4974f76ec7a
Ar0lona <nike@borheyer.de>
2026-09-26 11:31:36 +0200

    waldbrandseite

--- src/app/feuer/page.tsx
+++ src/app/feuer/page.tsx
@@ -3,7 +3,17 @@ import Link from "next/link";
 export default function SecondPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-900 px-6 py-16 text-center text-white">
-        <h1 className="mb-6 text-4xl font-bold">Waldbran</h1>
+        <h1 className="mb-6 text-4xl font-bold">Waldbrandgefahr</h1>
+        <div className="flex gap-6">
+          <div>
+            <h2>Wie entstehen Waldbrände?</h2>
+            <p>Waldbrände entstehen durch verschiedene Ursachen, darunter natürliche Faktoren wie Blitzschläge und menschliche Aktivitäten wie unkontrollierte Feuer oder Abbrand.</p>
+          </div>
+          <div>
+            <h2>Wie kann man Waldbrände verhindern?</h2>
+            <p>Um Waldbrände zu verhindern, ist es wichtig, vorsichtig mit Feuer umzugehen, keine offenen Flammen in Waldgebieten zu verwenden und auf die Einhaltung von Brandschutzvorschriften zu achten.</p>
+          </div>
+        </div>
       <Link
         href="/"
        className="rounded-full bg-white px-6 py-3 text-black transition hover:bg-zinc-300"
